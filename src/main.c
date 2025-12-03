53403d86fcb63ce38aa814c959198ecb83b5f30d
felipepimentab <felipe.pbernardo@gmail.com>
2025-12-03 12:12:09 -0300

    feat(sysbuild): add system build configuration files and version management
    - Add new sysbuild configuration files for mcuboot and ipc_radio
    - Introduce VERSION file for centralized version management
    - Replace version.h with print.h for version display
    - Remove deprecated bump_version.sh script
    - Update nrf5340dk overlay with external flash configuration

--- src/main.c
+++ src/main.c
@@ -1,30 +1,23 @@
+#include "application/controller.h"
+#include "print.h"
+#include "services/audio_codec/audio_codec.h"
+#include "services/bluetooth/bluetooth.h"
 #include "system_modules/peripheral/button.h"
 #include "system_modules/peripheral/led.h"
+#include "system_modules/storage/storage.h"
+#include "utils/macros_common.h"
 #include <ctype.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <zephyr/device.h>
-#include <zephyr/devicetree.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/i2c.h>
-#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/shell/shell.h>
-#include <zephyr/zbus/zbus.h>
-
-#include "application/controller.h"
-#include "services/audio_codec/audio_codec.h"
-#include "services/bluetooth/bluetooth.h"
-#include "system_modules/storage/storage.h"
-#include "utils/macros_common.h"
-#include "version.h"
 
 LOG_MODULE_REGISTER(Main_app, LOG_LEVEL_INF);
 
 int main(void)
 {
   int ret = 0;
-  LOG_INF("Tiresias Firmware v%s starting.", TIRESIAS_VERSION_STRING);
+  PRINT_TIRESIAS_BANNER();
+  PRINT_FIRMWARE_VERSION();
 
   ret = init_button();
   ERR_CHK(ret);
---
+++ src/print.h
@@ -0,0 +1,21 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#include <app_version.h>
+#include <zephyr/sys/printk.h>
+
+#define PRINT_TIRESIAS_BANNER()                                                                                        \
+  do {                                                                                                                 \
+    printk(" _____ _               _            \n");                                                                  \
+    printk("|_   _(_)_ __ ___  ___(_) __ _ ___  \n");                                                                  \
+    printk("  | | | | '__/ _ \\/ __| |/ _` / __| \n");                                                                 \
+    printk("  | | | | | |  __/\\__ \\ | (_| \\__ \\ \n");                                                              \
+    printk("  |_| |_|_|  \\___||___/_|\\__,_|___/ ");                                                                  \
+  } while (0)
+
+#define PRINT_FIRMWARE_VERSION()                                                                                       \
+  do {                                                                                                                 \
+    printk("Firmware v%s starting.\n\n", APP_VERSION_EXTENDED_STRING);                                                 \
+  } while (0)
+
+#endif // PRINT_H
